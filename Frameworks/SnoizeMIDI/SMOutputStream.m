65a670cbf40624a6202b04699b6905f84c471e28
Kurt Revis <krevis@snoize.com>
2002-01-04 15:19:04 +0000

    Fixed some bugs with the way we split long messages into packets. This is working better but still running into some lovely CoreMIDI bugs.

--- Frameworks/SnoizeMIDI/SMOutputStream.m
+++ Frameworks/SnoizeMIDI/SMOutputStream.m
@@ -71,35 +71,41 @@
 
 @implementation SMOutputStream (Private)
 
-const unsigned int maxPacketSize = 65535;
+const unsigned int maxPacketSize = 512;   //1010;
+    // NOTE: This should be 65535, since the storage for the packet length is an UInt16. However, large packets (> 1040 bytes) crash the MIDIServer quite reliably under 10.1.2.
+    // Also, the MIDIServer splits large packets into packets of <= 1010 bytes, so we might as well not make it do that. (1010 bytes of data in a message, packaged as one packet in a packet list, comes out to 1024 bytes of packet list.)
+    // TODO BUT! It seems that sending a single packet list of > 1024 bytes causes crashes, too, no matter how big the individual packets are. This is pretty stupid.  Need to look into it more.
 
 - (MIDIPacketList *)_packetListForMessages:(NSArray *)messages;
 {
     unsigned int messageIndex, messageCount;
     unsigned int packetListSize;
+    unsigned int packetCount;
     MIDIPacketList *packetList;
     MIDIPacket *packet;
     MIDITimeStamp now = 0;
 
     messageCount = [messages count];
     packetListSize = offsetof(MIDIPacketList, packet);
+    packetCount = 0;
 
     for (messageIndex = 0; messageIndex < messageCount; messageIndex++) {
         SMMessage *message;
         unsigned int otherDataLength;
-        unsigned int packetCount;
+        unsigned int messagePacketCount;
 
         message = [messages objectAtIndex:messageIndex];
         otherDataLength = [message otherDataLength];
         // Remember that all messages are at least 1 byte long; otherDataLength is on top of that.
 
         // Messages > maxPacketSize need to be split across multiple packets
-        packetCount = 1 + (1 + otherDataLength) / (maxPacketSize + 1);            
-        packetListSize += packetCount * offsetof(MIDIPacket, data) + 1 + otherDataLength;
+        messagePacketCount = 1 + (1 + otherDataLength) / (maxPacketSize + 1);            
+        packetListSize += messagePacketCount * offsetof(MIDIPacket, data) + 1 + otherDataLength;
+        packetCount += messagePacketCount;
     }
 
     packetList = (MIDIPacketList *)NSZoneMalloc(NSDefaultMallocZone(), packetListSize);
-    packetList->numPackets = messageCount;
+    packetList->numPackets = packetCount;
 
     if (flags.ignoresTimeStamps)
         now = AudioGetCurrentHostTime();
@@ -108,29 +114,29 @@ const unsigned int maxPacketSize = 65535;
     for (messageIndex = 0; messageIndex < messageCount; messageIndex++) {
         SMMessage *message;
         unsigned int otherDataLength;
-        unsigned int packetCount, packetIndex;
+        unsigned int messagePacketCount, messagePacketIndex;
         const Byte *messageData;
         unsigned int remainingLength;
         
         message = [messages objectAtIndex:messageIndex];
         otherDataLength = [message otherDataLength];
-        packetCount = 1 + (1 + otherDataLength) / (maxPacketSize + 1);
+        messagePacketCount = 1 + (1 + otherDataLength) / (maxPacketSize + 1);
 
         messageData = [message otherDataBuffer];
         remainingLength = 1 + otherDataLength;
 
-        for (packetIndex = 0; packetIndex < packetCount; packetIndex++) {
+        for (messagePacketIndex = 0; messagePacketIndex < messagePacketCount; messagePacketIndex++) {
             if (flags.ignoresTimeStamps)
                 packet->timeStamp = now;
             else
                 packet->timeStamp = [message timeStamp];
 
-            if (packetIndex + 1 == packetCount)		// last packet
+            if (messagePacketIndex + 1 == messagePacketCount)		// last packet for this message
                 packet->length = remainingLength;
             else
                 packet->length = maxPacketSize;
             
-            if (packetIndex == 0) {	
+            if (messagePacketIndex == 0) {	
                 // First packet needs special copying of status byte
                 packet->data[0] = [message statusByte];
                 if (packet->length > 1)
