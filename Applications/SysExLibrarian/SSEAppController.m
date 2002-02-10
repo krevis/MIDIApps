5991f8f7f6ac8adf5153735fd70b34397c633555
Kurt Revis <krevis@snoize.com>
2002-02-10 08:07:46 +0000

    Initialize CoreMIDI while the app's icon is still bouncing, so we don't have a large pause after it stops bouncing but before the app's window opens.  (CoreMIDI needs to find and possibly start its server process, which can take a while.)

--- Applications/SysExLibrarian/SSEAppController.m
+++ Applications/SysExLibrarian/SSEAppController.m
@@ -2,6 +2,7 @@
 
 #import <OmniBase/OmniBase.h>
 #import <OmniFoundation/OmniFoundation.h>
+#import <SnoizeMIDI/SnoizeMIDI.h>
 
 #import "SSEMainWindowController.h"
 #import "SSEPreferencesWindowController.h"
@@ -13,6 +14,13 @@
 // Application delegate
 //
 
+- (void)applicationWillFinishLaunching:(NSNotification *)notification;
+{
+    // Initialize CoreMIDI while the app's icon is still bouncing, so we don't have a large pause after it stops bouncing
+    // but before the app's window opens.  (CoreMIDI needs to find and possibly start its server process, which can take a while.)
+    [SMClient sharedClient];
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)notification;
 {
     // Make sure we go multithreaded, and that our scheduler starts up
@@ -23,10 +31,11 @@
 
 - (BOOL)application:(NSApplication *)sender openFile:(NSString *)filename;
 {
+    NSLog(@"open file: %@", filename);
+    
     // TODO
     // If the file is a library, need to use it
-    // If the file is a sysex file, need to add it to the library if it's not already there,
-    // and then select it in the library  (and perhaps play it?)
+    // If the file is a sysex file, need to add it to the library if it's not already there, and then select it in the library
     // NOTE: If the user double-clicks a file to launch us, this will get sent before -applicationDidFinishLaunching!
     // So we may need to remember the filename here and do something with it later.
     return NO;
