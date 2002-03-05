7f17941c34bbb397b6b9fc5f00a1019bece5d0c5
Kurt Revis <krevis@snoize.com>
2002-03-05 04:14:55 +0000

    Remove stupid header comment block.

--- Applications/SysExLibrarian/main.m
+++ Applications/SysExLibrarian/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  SysExLibrarian
-//
-//  Created by Kurt Revis on Sat Dec 29 2001.
-//  Copyright (c) 2001 __MyCompanyName__. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char *argv[])
