42ea49306dae6ca56d3735ed40b52470c1eafe51
Kurt Revis <krevis@snoize.com>
2002-03-13 02:59:18 +0000

    Added listener for data from SpyingMIDIDriver, using CFMessagePort.

--- Applications/MIDIMonitor/SMMAppController.m
+++ Applications/MIDIMonitor/SMMAppController.m
@@ -1,6 +1,7 @@
 #import "SMMAppController.h"
 
 #import <Cocoa/Cocoa.h>
+#import <CoreFoundation/CoreFoundation.h>
 #import <CoreMIDI/CoreMIDI.h>
 #import <OmniBase/OmniBase.h>
 #import <OmniFoundation/OmniFoundation.h>
@@ -14,7 +15,7 @@
 
 - (void)_endpointAppeared:(NSNotification *)notification;
 
-- (void)_spyNotification:(NSNotification *)notification;
+- (void)_connectToSpyingMIDIDriver;
 
 @end
 
@@ -51,7 +52,8 @@ NSString *SMMOpenWindowsForNewSourcesPreferenceKey = @"SMMOpenWindowsForNewSourc
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_endpointAppeared:) name:SMEndpointAppearedNotification object:nil];
 
     // TODO temporary
-    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(_spyNotification:) name:@"SpyingMIDIDriverNotification" object:nil suspensionBehavior:NSNotificationSuspensionBehaviorDeliverImmediately];
+    // hook up with the spying MIDI driver, if possible
+    [self _connectToSpyingMIDIDriver];
 }
 
 - (IBAction)showPreferences:(id)sender;
@@ -138,9 +140,76 @@ NSString *SMMOpenWindowsForNewSourcesPreferenceKey = @"SMMOpenWindowsForNewSourc
     }
 }
 
-- (void)_spyNotification:(NSNotification *)notification;
+static CFMessagePortRef localPort = NULL;
+static CFRunLoopSourceRef localPortRunLoopSource = NULL;
+
+
+static CFDataRef localMessagePortCallback(CFMessagePortRef local, SInt32 msgid, CFDataRef data, void *info)
 {
-    NSLog(@"Got spy notification: %@", notification);
+    const UInt8 *bytes;
+    SInt32 endpointUniqueID;
+    const char *endpointNameCString;
+    const MIDIPacketList *packetList;
+
+    if (!data || CFDataGetLength(data) < (sizeof(SInt32) + 1 + sizeof(UInt32))) {
+        NSLog(@"no data or not enough data: %@", data);
+        return NULL;
+    }
+    
+    bytes = CFDataGetBytePtr(data);
+
+    endpointUniqueID = *(SInt32 *)bytes;
+    endpointNameCString = (const char *)(bytes + sizeof(SInt32));
+    packetList = (const MIDIPacketList *)(bytes + sizeof(SInt32) + strlen(endpointNameCString) + 1);        
+
+    NSLog(@"got data from Spying MIDI Driver: unique ID %ld, name %s, packet list w/%lu packets",  endpointUniqueID, endpointNameCString, packetList->numPackets);
+
+    return NULL;
+}
+
+- (void)_connectToSpyingMIDIDriver;
+{
+    CFMessagePortRef spyingMIDIDriverMessagePort;
+    SInt32 sendStatus;
+    CFDataRef sequenceNumberData;
+
+    spyingMIDIDriverMessagePort = CFMessagePortCreateRemote(kCFAllocatorDefault, CFSTR("Spying MIDI Driver"));
+    if (!spyingMIDIDriverMessagePort) {
+        NSLog(@"couldn't find message port for Spying MIDI Driver");
+        return;
+    }
+
+    // ask for the next sequence number
+    sendStatus = CFMessagePortSendRequest(spyingMIDIDriverMessagePort, 0, NULL, 300, 300, kCFRunLoopDefaultMode, &sequenceNumberData);
+    if (sendStatus != kCFMessagePortSuccess) {
+        NSLog(@"CFMessagePortSendRequest(0) returned error: %ld", sendStatus);
+    } else if (!sequenceNumberData) {
+        NSLog(@"CFMessagePortSendRequest(0) returned no data");
+    } else if (CFDataGetLength(sequenceNumberData) != sizeof(UInt32)) {
+        NSLog(@"CFMessagePortSendRequest(0) returned %lu bytes, not %lu", CFDataGetLength(sequenceNumberData), sizeof(UInt32));
+    } else {
+        UInt32 sequenceNumber;
+        NSString *localPortName;
+
+        // Now get the sequence number and use it to name a newly created local port
+        sequenceNumber = *(UInt32 *)CFDataGetBytePtr(sequenceNumberData);
+        localPortName = [NSString stringWithFormat:@"Spying MIDI Driver-%lu", sequenceNumber];
+
+        localPort = CFMessagePortCreateLocal(kCFAllocatorDefault, (CFStringRef)localPortName, localMessagePortCallback, NULL, FALSE);
+        if (!localPort) {
+            NSLog(@"CFMessagePortCreateLocal failed!");
+        } else {
+            // Add the local port to the run loop
+            localPortRunLoopSource = CFMessagePortCreateRunLoopSource(kCFAllocatorDefault, localPort, 0);
+            CFRunLoopAddSource(CFRunLoopGetCurrent(), localPortRunLoopSource, kCFRunLoopDefaultMode);
+            
+            // And now tell the spying driver to add us as a listener (don't wait for a response)
+            sendStatus = CFMessagePortSendRequest(spyingMIDIDriverMessagePort, 1, sequenceNumberData, 300, 0, NULL, NULL);
+            if (sendStatus != kCFMessagePortSuccess) {
+                NSLog(@"CFMessagePortSendRequest(1) returned error: %ld", sendStatus);
+            }
+        }
+    }
 }
 
 @end
