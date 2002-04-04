9e3feb393e207204b74cf99d1ed42bd44aab7e1b
Kurt Revis <krevis@snoize.com>
2002-04-04 13:46:03 +0000

    Changed a constant name: sequenceNumber -> identifier.

--- Frameworks/SnoizeMIDISpy/MIDISpyShared.h
+++ Frameworks/SnoizeMIDISpy/MIDISpyShared.h
@@ -4,7 +4,7 @@
 
 // IDs of messages sent from client to driver via CFMessagePort
 enum {
-    kSpyingMIDIDriverNextSequenceNumberMessageID = 0,
+    kSpyingMIDIDriverGetNextListenerIdentifierMessageID = 0,
     kSpyingMIDIDriverAddListenerMessageID = 1,
     kSpyingMIDIDriverConnectDestinationMessageID = 2,
     kSpyingMIDIDriverDisconnectDestinationMessageID = 3
