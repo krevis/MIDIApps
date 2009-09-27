a10e55fd9508d19c4b5fd7c8f506a7adbd2e5d9a
Kurt Revis <krevis@snoize.com>
2009-09-27 10:43:15 +0000

    Added -endpoint so people can check against -destinationEndpoint in messages

--- Frameworks/SnoizeMIDI/SMVirtualInputStream.h
+++ Frameworks/SnoizeMIDI/SMVirtualInputStream.h
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2001-2004, Kurt Revis.  All rights reserved.
+ Copyright (c) 2001-2009, Kurt Revis.  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  
@@ -36,5 +36,7 @@
 - (void)setVirtualEndpointName:(NSString *)value;
 
 - (void)setInputSourceName:(NSString *)value;
+
+- (SMDestinationEndpoint*)endpoint;
 
 @end
--- Frameworks/SnoizeMIDI/SMVirtualInputStream.m
+++ Frameworks/SnoizeMIDI/SMVirtualInputStream.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2001-2004, Kurt Revis.  All rights reserved.
+ Copyright (c) 2001-2009, Kurt Revis.  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  
@@ -97,6 +97,11 @@
 - (void)setInputSourceName:(NSString *)value;
 {
     [inputStreamSource setName:value];
+}
+
+- (SMDestinationEndpoint*)endpoint
+{
+    return endpoint;
 }
 
 //
