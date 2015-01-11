2d0252e0c0ccea7a56e551facda19d1a7be8badb
Kurt Revis <krevis@snoize.com>
2015-01-11 12:37:48 -0800

    Hook up SnoizeMIDI in the Swift app delegate, proof of concept

--- Applications/SwiftApp/SwiftApp/AppDelegate.swift
+++ Applications/SwiftApp/SwiftApp/AppDelegate.swift
@@ -7,21 +7,39 @@
 //
 
 import Cocoa
+import SnoizeMIDI
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, SMMessageDestination {
 
     @IBOutlet weak var window: NSWindow!
 
+    var inputStream: SMPortInputStream!
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+
+        // Make a CoreMIDI client via SnoizeMIDI
+        var client: SMClient? = SMClient.sharedClient();
+        println("SMClient is \(client)")
+
+        // Make a SnoizeMIDI input stream
+        inputStream = SMPortInputStream()
+        // that calls our takeMIDIMessages() method
+        inputStream.setMessageDestination(self)
+        // and listens to all currently available input sources
+        inputStream.setSelectedInputSources(NSSet(array: inputStream.inputSources()))
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
-
+    func takeMIDIMessages(messages: [AnyObject]!) {
+        // Print each message as it is received
+        for message in messages {
+            println("Received MIDI message \(message.typeForDisplay())");
+        }
+    }
 }
 
