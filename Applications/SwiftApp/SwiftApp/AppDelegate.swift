f7030ba0bfaaa5157f0dd52c7822b047df23bcd8
Kurt Revis <krevis@snoize.com>
2015-01-11 12:36:02 -0800

    New Swift app project, straight from the Xcode template

---
+++ Applications/SwiftApp/SwiftApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftApp
+//
+//  Created by Kurt Revis on 1/11/15.
+//  Copyright (c) 2015 Kurt Revis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
