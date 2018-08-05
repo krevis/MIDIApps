59c9ace8ab33bd70e024003e28872863de8a4bf2
Kurt Revis <krevis@snoize.com>
2018-08-04 18:52:22 -0700

    We really shouldn't need to import Carbon.h anymore

--- Applications/SysExLibrarian/PrecompHeader.h
+++ Applications/SysExLibrarian/PrecompHeader.h
@@ -11,5 +11,4 @@
  */
 
 #import <Cocoa/Cocoa.h>
-#import <Carbon/Carbon.h>
 #import <CoreMIDI/CoreMIDI.h>
--- Applications/SysExLibrarian/SSELibrary.m
+++ Applications/SysExLibrarian/SSELibrary.m
@@ -12,7 +12,6 @@
 
 #import "SSELibrary.h"
 
-#import <Carbon/Carbon.h>
 #import <SnoizeMIDI/SnoizeMIDI.h>
 
 #import "SSELibraryEntry.h"
