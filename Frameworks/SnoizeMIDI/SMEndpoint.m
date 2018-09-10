ab25efe6e738bc6e0e3143d0ef5d6034feaea955
Kurt Revis <krevis@snoize.com>
2018-09-10 00:40:01 -0700

    copyright dates

--- Frameworks/SnoizeMIDI/SMEndpoint.m
+++ Frameworks/SnoizeMIDI/SMEndpoint.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2001-2009, Kurt Revis.  All rights reserved.
+ Copyright (c) 2001-2018, Kurt Revis.  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  
