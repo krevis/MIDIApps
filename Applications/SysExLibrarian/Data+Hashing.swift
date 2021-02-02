31a7306333a72a30feae1c9686ba48c998a7aa16
Kurt Revis <krevis@snoize.com>
2021-02-01 21:47:06 -0800

    Turn on SwiftLint for SEL, fix a few little things

--- Applications/SysExLibrarian/Data+Hashing.swift
+++ Applications/SysExLibrarian/Data+Hashing.swift
@@ -11,31 +11,31 @@ import CommonCrypto
 // TODO Extend Data not NSData
 @objc extension NSData {
 
-    @objc var md5HexHash: String {
-        var digest = Array<UInt8>(repeating: 0, count: 16)
+    var md5HexHash: String {
+        var digest = [UInt8](repeating: 0, count: 16)
 
         digest.withUnsafeMutableBufferPointer { ( digestBufferPtr: inout UnsafeMutableBufferPointer<UInt8>) -> Void in
             CC_MD5(self.bytes, CC_LONG(self.length), digestBufferPtr.baseAddress)
         }
 
         var result = ""
-        for i in 0 ..< 16 {
-            result += String(format: "%02x", digest[i])
+        for index in 0 ..< 16 {
+            result += String(format: "%02x", digest[index])
         }
 
         return result
     }
 
-    @objc var sha1HexHash: String {
-        var digest = Array<UInt8>(repeating: 0, count: 16)
+    var sha1HexHash: String {
+        var digest = [UInt8](repeating: 0, count: 16)
 
         digest.withUnsafeMutableBufferPointer { ( digestBufferPtr: inout UnsafeMutableBufferPointer<UInt8>) -> Void in
             CC_SHA1(self.bytes, CC_LONG(self.length), digestBufferPtr.baseAddress)
         }
 
         var result = ""
-        for i in 0 ..< 16 {
-            result += String(format: "%02x", digest[i])
+        for index in 0 ..< 16 {
+            result += String(format: "%02x", digest[index])
         }
 
         return result
