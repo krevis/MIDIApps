2eb1322bf76a2a527aec7196f1a1661fc126c5f0
Kurt Revis <krevis@snoize.com>
2021-02-19 15:45:14 -0800

    Updated strings. Added a script to do it.

---
+++ Scripts/update-localized-strings.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+#
+# Run this from the MIDIApps directory as "./Scripts/update-localized-strings.sh"
+
+pushd Applications/MIDIMonitor	
+genstrings *.swift *.m -o en.lproj
+popd
+
+pushd Applications/SysExLibrarian
+genstrings *.swift -o en.lproj
+popd
+
+pushd Frameworks/SnoizeMIDI
+genstrings *.swift *.m -o en.lproj
+popd
