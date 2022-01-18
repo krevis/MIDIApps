2651a12c47fc1a4e2119fa82835e1c4581b4f41d
Kurt Revis <krevis@snoize.com>
2022-01-17 19:39:49 -0800

    Bump MM version to 1.5.3

--- Frameworks/SnoizeMIDI/Message.swift
+++ Frameworks/SnoizeMIDI/Message.swift
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2001-2021, Kurt Revis.  All rights reserved.
+ Copyright (c) 2001-2022, Kurt Revis.  All rights reserved.
 
  This source code is licensed under the BSD-style license found in the
  LICENSE file in the root directory of this source tree.
