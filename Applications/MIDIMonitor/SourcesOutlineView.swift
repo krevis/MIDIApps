9f95a2ca3e46bf15e13bd2ba8843930322fecb08
Kurt Revis <krevis@snoize.com>
2022-09-02 20:22:28 -0700

    Fix deprecation warning about event.context being deprecated and always nil on 10.12 and later

--- Applications/MIDIMonitor/SourcesOutlineView.swift
+++ Applications/MIDIMonitor/SourcesOutlineView.swift
@@ -13,7 +13,7 @@ class SourcesOutlineView: NSOutlineView {
         // Ignore all double-clicks (and triple-clicks and so on) by pretending they are single-clicks.
         var modifiedEvent: NSEvent?
         if event.clickCount > 1 {
-            modifiedEvent = NSEvent.mouseEvent(with: event.type, location: event.locationInWindow, modifierFlags: event.modifierFlags, timestamp: event.timestamp, windowNumber: event.windowNumber, context: event.context, eventNumber: event.eventNumber, clickCount: 1, pressure: event.pressure)
+            modifiedEvent = NSEvent.mouseEvent(with: event.type, location: event.locationInWindow, modifierFlags: event.modifierFlags, timestamp: event.timestamp, windowNumber: event.windowNumber, context: nil, eventNumber: event.eventNumber, clickCount: 1, pressure: event.pressure)
         }
 
         super.mouseDown(with: modifiedEvent ?? event)
